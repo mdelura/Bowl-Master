c5b4a54921e5cffe1e4d451954d8d9566aa3dc16
Michal Delura <deluramichal@gmail.com>
2018-02-10 00:36:37 +0100

    Set pins, add ball and following camera

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Ball : MonoBehaviour
+{
+    public float throwPower = 150;
+
+    private Rigidbody _rigidbody;
+    private AudioSource _audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
+
+        Launch();
+    }
+
+    public void Launch()
+    {
+        _rigidbody.velocity = Vector3.forward * throwPower;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    bool _audioPlayed;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!_audioPlayed)
+        {
+            _audioSource.Play();
+            _audioPlayed = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private const float zLimit = 1829;
+
+    private Ball _ball;
+    private Vector3 _offfset;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        _ball = FindObjectOfType<Ball>();
+        _offfset = transform.position - _ball.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < zLimit)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, _ball.transform.position.z + _offfset.z);
+            transform.position = _ball.transform.position + _offfset;
+        }
+    }
+}
