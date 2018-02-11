55c6518aca8f9f63ac96a2cdc030dc863398f54c
Michal Delura <deluramichal@gmail.com>
2018-02-11 16:57:42 +0100

    Add DragLaunch logic

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,29 +5,42 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Ball : MonoBehaviour
 {
-    public float throwPower;
-
     private Rigidbody _rigidbody;
     private AudioSource _audioSource;
+
 
     // Use this for initialization
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.useGravity = false;
         _audioSource = GetComponent<AudioSource>();
-
-        Launch();
     }
 
-    public void Launch()
+    const float speedDistance = 18.29f;
+    float _launchStart;
+    float _zStart;
+    bool _speedShown;
+
+    public void Launch(Vector3 velocity)
     {
-        _rigidbody.velocity = Vector3.forward * throwPower + new Vector3(Random.Range(-0.1f, 0.1f), 0);
+        _launchStart = Time.realtimeSinceStartup;
+        _zStart = transform.position.z;
+        _rigidbody.useGravity = true;
+        _rigidbody.velocity = velocity;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (transform.position.z >= 18.29f && !_speedShown)
+        {
+            float distance = transform.position.z - _zStart;
+            float duration = Time.realtimeSinceStartup - _launchStart;
+            float speed = distance / duration;
+            print($"Distance: {distance}, Duration: {duration}, Speed: {speed} m/s, {speed / 3.6F} km/h.");
+            _speedShown = true;
+        }
     }
 
     bool _audioPlayed;
---
+++ Assets/Scripts/DragLaunch.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(Ball))]
+public class DragLaunch : MonoBehaviour
+{
+    const float realSpeedFactor = 0.01878f;
+
+    private float _usedScreen = 0.8f;
+
+    private Ball _ball;
+    // Use this for initialization
+    void Start()
+    {
+        _ball = GetComponent<Ball>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private float _dragStartTime;
+    private Vector3 _dragStartPosition;
+
+    public void DragStart()
+    {
+        //Capture time & position of drag start
+        _dragStartTime = Time.realtimeSinceStartup;
+        _dragStartPosition = Input.mousePosition;
+    }
+
+
+    private List<float> _ySpeed = new List<float>();
+
+    public void DragEnd()
+    {
+        //Calculate velocity and launch the ball
+        float dragDuration = Time.realtimeSinceStartup - _dragStartTime;
+        Vector3 dragVelocity = (Input.mousePosition - _dragStartPosition) / dragDuration;// * realSpeedFactor;
+
+        Vector3 launchVelocity = new Vector3(dragVelocity.x, 0, dragVelocity.y );
+
+        //DEBUG
+        _ySpeed.Add(dragVelocity.y / dragDuration);
+        print($"Average ySpeed is: {_ySpeed.Average()} m/s, {_ySpeed.Average() /3.6f} km/h, current throw {launchVelocity.z/3.6f} km/h");
+
+        //print($"Start position X: {_dragStartPosition.x}, Y: {_dragStartPosition.y}  Drag time: {dragTime}, Velocity: X: {throwVelocity.x}, Z: {throwVelocity.z}");
+
+
+        _ball.Launch(launchVelocity);
+
+
+    }
+}
